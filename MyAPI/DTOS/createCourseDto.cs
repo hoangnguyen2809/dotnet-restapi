c6b283b344bbaf464c3790899699f28db0eb27b3
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-11 18:51:10 -0700

    name changed

--- MyAPI/DTOS/createCourseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyAPI.DTOS;
-
-public record class createCourseDto(
-    string courseName,
-    string courseCode,
-    string courseDescription,
-    int credits,
-    string instructor
-);
--- MyAPI/DTOS/updateCourseDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyAPI;
-
-public record class createCourseDto(
-    string courseName,
-    string courseCode,
-    string courseDescription,
-    int credits,
-    string instructor
-);
