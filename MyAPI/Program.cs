4f5f374a6de574eda1643c3d6c79e5c5c16d841b
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-12 14:59:22 -0700

    config connection string for sqlite

--- MyAPI/Program.cs
+++ MyAPI/Program.cs
@@ -3,8 +3,13 @@ using MyAPI.DTOs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var connectionString = builder.Configuration.GetConnectionString("MyAPI");
+builder.Services.AddSqlite<Context>(connectionString);
+
 var app = builder.Build();
 
 app.MapCoursesEndpoints();
+
+app.MigrateDatabase();
 
 app.Run();
