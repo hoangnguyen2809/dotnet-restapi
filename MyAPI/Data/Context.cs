ce49286b081b5c4a185bca492bc27b312d0e099c
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-11 19:52:22 -0700

    Create DBContext

---
+++ MyAPI/Data/Context.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAPI.Entities;
+
+namespace MyAPI.Data;
+
+public class Context(DbContextOptions<Context> options) : DbContext(options)
+{
+    public DbSet<Course> Courses => Set<Course>();
+    public DbSet<Department> Departments => Set<Department>();
+}
