dbac6767fe5b93965f05981e49f6c3190d8f6879
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-11 19:51:34 -0700

    Created entities

---
+++ MyAPI/Entities/Course.cs
@@ -0,0 +1,14 @@
+﻿namespace MyAPI.Entities;
+
+public class Course
+{
+    public int id { get; set; }
+    public required string courseCode { get; set; }
+    public int departmentId { get; set; }
+    public required Department department { get; set; }
+
+    public string? courseDescription { get; set; }
+    public int credits { get; set; }
+
+    public int instructor { get; set; }
+}
---
+++ MyAPI/Entities/department.cs
@@ -0,0 +1,7 @@
+﻿namespace MyAPI.Entities;
+
+public class Department
+{
+    public int id { get; set; }
+    public required string name { get; set; }
+}
