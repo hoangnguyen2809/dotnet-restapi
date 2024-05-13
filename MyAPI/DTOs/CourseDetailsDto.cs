e202f12a485032a80fd2a312198dfa9198feb735
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-12 17:45:30 -0700

    details dto for displaying faculty id

---
+++ MyAPI/DTOs/CourseDetailsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MyAPI.DTOs;
+
+public record class CourseDetailsDto(
+    int id,
+    int facultyId,
+    string courseCode,
+    string? courseDescription,
+    int credits,
+    string? instructor
+);
