c501f16efb5ee89b9989eac01fad44e0b3e6cf34
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-12 18:13:53 -0700

    add faculty mapping

---
+++ MyAPI/Mapping/FacultyMapping.cs
@@ -0,0 +1,12 @@
+﻿using MyAPI.DTOs;
+using MyAPI.Entities;
+
+namespace MyAPI.Mapping;
+
+public static class FacultyMapping
+{
+    public static FacultyDto ToDto(this Faculty faculty)
+    {
+        return new FacultyDto(faculty.id, faculty.name);
+    }
+}
