0c8fc6c648ca7e723e6ff259ea41723c4d41c0a8
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-12 18:13:39 -0700

    add faculty endpoints

---
+++ MyAPI/Endpoints/FacultyEndpoints.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyAPI.Data;
+using MyAPI.Mapping;
+
+namespace MyAPI.Endpoints;
+
+public static class FacultyEndpoints
+{
+    public static RouteGroupBuilder MapFacultiesEndpoints(this WebApplication app)
+    {
+        var group = app.MapGroup("faculties").WithParameterValidation();
+
+        group.MapGet(
+            "/",
+            async (Context dbContext) =>
+                await dbContext
+                    .Faculties.Select(faculty => faculty.ToDto())
+                    .AsNoTracking()
+                    .ToListAsync()
+        );
+
+        return group;
+    }
+}
