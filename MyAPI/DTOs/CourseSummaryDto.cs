781b759e168fb6f3bbbcef40a13fb3fec38e29d9
anhhoang289 <anhhoang289@hoangmyUbuntu20.04.hoangmy.virtualbox.org>
2024-05-12 17:45:57 -0700

    update CourseDto.cs

---
+++ MyAPI/DTOs/CourseSummaryDto.cs
@@ -0,0 +1,10 @@
+﻿namespace MyAPI.DTOs;
+
+public record class CourseSummaryDto(
+    int id,
+    string faculty,
+    string courseCode,
+    string? courseDescription,
+    int credits,
+    string? instructor
+);
